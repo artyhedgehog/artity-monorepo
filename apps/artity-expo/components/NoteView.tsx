80ad9dcd4765f84d3467c12e06eee73b6c793250
Artem Kolomytsev <arhed@ya.ru>
2023-03-12 11:53:54 +0300

    wip(artity-expo): handle denote url

--- apps/artity-expo/components/NoteView.tsx
+++ apps/artity-expo/components/NoteView.tsx
@@ -1,19 +1,39 @@
-import React from 'react'
-import { Box, TextArea, Button, Input, HStack } from "native-base";
+import React, { useEffect } from 'react'
+import * as Linking from 'expo-linking'
+import { Box, TextArea, Button, Input, HStack } from "native-base"
+
+const linkUrl = 'denote://what/202303091908'
+/* const linkUrl = 'exp://192.168.0.111:19000' */
 
 export default function NoteView() {
-    const [title, setTitle] = React.useState('Note title')
+    const url = Linking.useURL()
+
+    useEffect(() => {
+        setTitle(url)
+    }, [url])
+
+    const [title, setTitle] = React.useState(url)
     const [content, setContent] = React.useState('* Note content')
 
     return (
         <Box>
-            <Input value={title} onChangeText={setTitle} placeholder="Note title" required/>
+            <Input
+                value={title}
+                onChangeText={setTitle}
+                placeholder="Note title"
+            />
 
-            <TextArea flex={1} placeholder="Note description" w="100%" value={content} onChangeText={setContent} />
+            <TextArea
+                flex={1}
+                placeholder="Note description"
+                w="100%"
+                value={content}
+                onChangeText={setContent}
+            />
 
             <HStack space={2}>
-                <Button flex={1}>
-                    Save
+                <Button flex={1} onPress={ () => Linking.openURL(linkUrl) }>
+                    Save denote
                 </Button>
 
                 <Button flex={1}>
@@ -21,6 +41,5 @@ export default function NoteView() {
                 </Button>
             </HStack>
         </Box>
-
     )
 }
